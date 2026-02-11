568b28c165fb3d1e6102448e10e662b8691a8090
Greg Roach <greg@subaqua.co.uk>
2026-02-11 09:18:46 +0000

    Ensure CLI commands only run from the CLI

--- app/Cli/Commands/AbstractCommand.php
+++ app/Cli/Commands/AbstractCommand.php
@@ -20,6 +20,7 @@ declare(strict_types=1);
 namespace Fisharebest\Webtrees\Cli\Commands;
 
 use InvalidArgumentException;
+use LogicException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 
@@ -28,6 +29,15 @@ use function is_string;
 
 abstract class AbstractCommand extends Command
 {
+    public function __construct()
+    {
+        if (PHP_SAPI !== 'cli') {
+            throw new LogicException('Commands can only run from the command line.');
+        }
+
+        parent::__construct();
+    }
+
     protected function boolOption(InputInterface $input, string $name): bool
     {
         $value = $input->getOption(name: $name);
